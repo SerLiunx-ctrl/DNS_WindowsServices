9bb263cb62b5335890768f4b544c17bba073527c
BuildTools <unconfigured@null.spigotmc.org>
2021-04-22 10:29:08 +0800

    master

---
+++ DNS_WindowsServices/Files/EXml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml;
+
+namespace DNS_WindowsServices.Files
+{
+    class EXml
+    {
+        public string path { get; set; }
+
+        public string name { get; set; }
+
+        XmlDocument xmldoc;
+
+        public EXml(string path,string name)
+        {
+            this.path = path;
+            this.name = name;
+
+            xmldoc = new XmlDocument();
+        }
+
+        //创建xml文档
+        public void CreateXml(string root,string version,string ecoding)
+        {
+            if (xmldoc.Equals(null))
+                return;
+            XmlDeclaration xmldecl;
+            XmlElement xmlelem;
+
+            xmldecl = xmldoc.CreateXmlDeclaration(version, ecoding, null);
+            xmldoc.AppendChild(xmldecl);
+            xmlelem = xmldoc.CreateElement("", root, "");
+            xmldoc.AppendChild(xmlelem);
+        }
+    }
+}
--- DNS_WindowsServices/Utils/Service.cs
+++ DNS_WindowsServices/Utils/Service.cs
@@ -12,13 +12,13 @@ namespace DNS_WindowsServices.Utils
         //服务启动
         protected override void OnStart(string[] args)
         {
-            _ins = new List<Instance>();
             _insfiles = new InstanceFiles();
             _insfiles.LoadFromFiles();
             _ins = _insfiles.GetInstances();
 
-            foreach (var t in _ins)
-                t.Start();
+            foreach (var i in _ins)
+                i.Start();
+
             Log.OutLine("服务启动成功!");
             base.OnStart(args);
         }
