9441dd3c5afd06a18a098d686531f70462e9d708
BuildTools <unconfigured@null.spigotmc.org>
2021-04-25 22:57:07 +0800

    添加控制器

---
+++ Controller/Controller.cs
@@ -0,0 +1,18 @@
+﻿using Controller.Utils;
+using System;
+using System.Collections;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Controller
+{
+    class Controller
+    {
+        static void Main(string[] args)
+        {
+  
+            MainInstaller a = new MainInstaller();
+            a.Install(new ProDictionary());
+        }
+    }
+}
---
+++ Controller/Utils/Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+
+namespace Controller.Utils
+{
+    class Service
+    {
+        public string serviceName { get; set; }
+        private ServiceController serviceController;
+
+        public Service(string serviceName)
+        {
+            this.serviceName = serviceName;
+
+            foreach (ServiceController sc in ServiceController.GetServices())
+            {
+                if (sc.DisplayName.Equals(this.serviceName))
+                {
+                    this.serviceController = sc;
+                    break;
+                }
+            }
+        }
+    }
+}
